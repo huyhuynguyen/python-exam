c2e8673932cb70b0330921eeec913f3de1221201
huyhuynguyen <quangquanghuy26122000@gmail.com>
2022-06-01 09:44:50 +0700

    fix(enums): remove __all__ in __init__.py

--- enums/__init__.py
+++ enums/__init__.py
@@ -1,4 +1,2 @@
 from enums.guess_choice import GuessText
 from enums.constants import *
-
-__all__ = ['TIME_SLEEP']
--- enums/guess_choice.py
+++ enums/guess_choice.py
@@ -4,10 +4,15 @@ class GuessText(enum.Enum):
     G = 'greater'
     L = 'less'
 
-# if __name__ == '__main__':
-#     string = 'G'
-#     print(GuessText[string].value)
-#     print([text.value for text in GuessText])
-#     print(GuessText('greater').name)
-#     print(GuessText.G.value)
-#     print([GuessText(text.value).name for text in GuessText])
+if __name__ == '__main__':
+    # get list values
+    print([text.value for text in GuessText])
+
+    # get key
+    print(GuessText('greater').name)
+
+    # get value
+    print(GuessText.G.value)
+
+    # get list keys
+    print([GuessText(text.value).name for text in GuessText])
