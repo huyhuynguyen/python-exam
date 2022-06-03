3d6c62d9c92ca1ae05749e6cd2bd34ce587f1462
huyhuynguyen <quangquanghuy26122000@gmail.com>
2022-06-03 10:06:06 +0700

    fix(test): fix testcase deck and add .converagerc file

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -9,7 +9,7 @@ class TestDeck(unittest.TestCase):
 
     def test_get_random_card(self):
         res = self.deck.get_random_card()
-        self.assertIn(res['card'], self.deck.card_list)
+        self.assertNotIn(res['card'], self.deck.card_list)
 
 if __name__ == '__main__':
     subprocess.run(['pytest', '-v', r'tests/test_deck.py'], shell = True)
