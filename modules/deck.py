9e53b02282bcaa8848acf6fb88b43aca99bfbd7b
quanghuy <quangquanghuy26122000@gmail.com>
2022-06-09 00:29:16 +0700

    fix(deck): replace symbol card

--- modules/deck.py
+++ modules/deck.py
@@ -2,13 +2,13 @@ import random
 
 class Deck:
     def __init__(self) -> None:
-        self.suites = ['spade', 'club', 'diamond', 'heart']
+        self.suites = ['\u2660', '\u2663', '\u2666', '\u2665']
         self.groups = ['A'] + [str(x) for x in range(2,11)] + ['J', 'Q', 'K']
         self.greatest_card = ['b-joker', 'r-joker']
         self._card_list = self.get_card_list()
 
     def get_card_list(self):
-        card_list_name = [str(x)+'-'+str(y) for x in self.groups for y in self.suites] \
+        card_list_name = [str(x)+str(y) for x in self.groups for y in self.suites] \
                 + self.greatest_card
 
         return [{
@@ -32,12 +32,3 @@ class Deck:
         # remove card after choose
         self.card_list = [card_item for card_item in self.card_list if card_item != card]
         return card
-
-if __name__ == '__main__':
-    deck = Deck()
-    print(deck.card_list)
-    print("\u2764\uFE0F")
-    print("Spades -> \u2660")
-    print("Club -> \u2663")
-    print("Diamond -> \u2666")
-    print("Heart -> \u2665")
--- note-todo.md
+++ note-todo.md
@@ -4,6 +4,3 @@ TODO
 - refactor decrease card in deck after get random card
 - print 3 2 1 ... next round after player loses
 - out of card -> reset
-
-PROCESSING
-- add python symbol: 'spade', 'club', 'diamond', 'heart'
